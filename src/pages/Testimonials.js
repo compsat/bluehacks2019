4e70b357483b08a8c74b5c2e87a393d2037606be
Daniel <daniel.delrio@obf.ateneo.edu>
2020-01-27 10:24:19 +0800

    Update About, Contact, Home, Map and Testimonials

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -6,22 +6,38 @@ export default class Testimonials extends React.Component {
 			<div id="testimonials-container">
 				<h1>Testimonials</h1>
 				<div class="main">
-					<div class="photo"><img alt="Brian Tan" src={require('../assets/brian.jpg')} /></div>
+					<div class="photo"><img alt="Neriz Broqueza" src={require('../assets/brian.jpg')} /></div>
 					<div class="content">
 						<p>
-							Blue Hacks was a life-changing experience for me. It was so 
-							exciting to form a team, figure out how to solve a real problem, 
-							and build and pitch an app within 24 hours. It's where we first 
-							ideated and prototyped HangTime, which we launched 10 months later 
-							and got thousands of students who've used it and love it. I highly 
-							recommend Blue Hacks for anyone who wants to accelerate their learning 
-							of Web and App Development!
+							There was so much that I learned in the event. I felt some pressure going 
+							in the hackathon, since it was my first time where I needed to pitch my team’s 
+							idea to the panel of judges. As a team, we treated ourselves not as the developer 
+							but as a user of our application. Thanks to the words of encouragement I got from 
+							my teammates, as well as the teamwork and cooperation we had, we were able to 
+							successfully develop our application, and I was able to defend my team’s program 
+							to the panel. Being able to meet with the various teams and seeing their ideas 
+							was also a very memorable experience! All in all, the experience in Blue Hacks 
+							is well worth enjoying!
 						</p>
 						<div>
-							<span class="blue"><b>Brian Tan</b></span> <br />
-							<span class="pink">Member of Team Growth Hackers </span><br />
-							<span class="orange">Blue Hacks 2016 2nd place winner </span><br />
+							<span class="blue"><b>Neriz Broqueza</b></span> <br />
+							<span class="pink">Member of Team Med Hack</span><br />
+							<span class="orange">Blue Hacks 2020 3rd place winner </span><br />
 							<a class="purple" href="https://hangtimeapp.com" target="_blank" rel="noopener noreferrer">hangtimeapp.com</a>
+						</div>
+					</div>
+				</div>
+				<div class="main">
+					<div class="photo"><img alt="Joaquin Jacinto" src={require('../assets/joaquin.jpg')} /></div>
+					<div class="content">
+						<p>
+							Our group made an e-commerce app called "PaSELLubong" that allows indigenous people to sell their products and gives the buyers a place to learn more about their culture. What I liked the most from Blue Hacks was the theme because it allowed the students to think of various ways that technology could help build up our country as a nation. Many of the apps had great potential to be continued outside of the event so that there could be an actual impact to our nation.
+						</p>
+						<div>
+							<span class="blue"><b>Joaquin Jacinto</b></span> <br />
+							<span class="pink">Member of Team ??? </span> <br />
+							<span class="orange">Blue Hacks 2018 1st place winner </span> <br />
+							<a class="purple" href="https://github.com/Albdog/PaSELLubong" target="_blank" rel="noopener noreferrer">github.com/Albdog/PaSELLubong</a>
 						</div>
 					</div>
 				</div>
@@ -59,16 +75,22 @@ export default class Testimonials extends React.Component {
 					</div>
 				</div>
 				<div class="main">
-					<div class="photo"><img alt="Joaquin Jacinto" src={require('../assets/joaquin.jpg')} /></div>
+					<div class="photo"><img alt="Brian Tan" src={require('../assets/brian.jpg')} /></div>
 					<div class="content">
 						<p>
-							Our group made an e-commerce app called "PaSELLubong" that allows indigenous people to sell their products and gives the buyers a place to learn more about their culture. What I liked the most from Blue Hacks was the theme because it allowed the students to think of various ways that technology could help build up our country as a nation. Many of the apps had great potential to be continued outside of the event so that there could be an actual impact to our nation.
+							Blue Hacks was a life-changing experience for me. It was so 
+							exciting to form a team, figure out how to solve a real problem, 
+							and build and pitch an app within 24 hours. It's where we first 
+							ideated and prototyped HangTime, which we launched 10 months later 
+							and got thousands of students who've used it and love it. I highly 
+							recommend Blue Hacks for anyone who wants to accelerate their learning 
+							of Web and App Development!
 						</p>
 						<div>
-							<span class="blue"><b>Joaquin Jacinto</b></span> <br />
-							<span class="pink">Member of Team ??? </span> <br />
-							<span class="orange">Blue Hacks 2018 1st place winner </span> <br />
-							<a class="purple" href="https://github.com/Albdog/PaSELLubong" target="_blank" rel="noopener noreferrer">github.com/Albdog/PaSELLubong</a>
+							<span class="blue"><b>Brian Tan</b></span> <br />
+							<span class="pink">Member of Team Growth Hackers </span><br />
+							<span class="orange">Blue Hacks 2016 2nd place winner </span><br />
+							<a class="purple" href="https://hangtimeapp.com" target="_blank" rel="noopener noreferrer">hangtimeapp.com</a>
 						</div>
 					</div>
 				</div>
