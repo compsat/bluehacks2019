5b63827841de988dc27bb9bf39e035da33751c6d
Cobbie Quintos <cobbiequintos@gmail.com>
2020-01-28 18:34:39 +0800

    update logos missing AIPO and habitat

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -7,13 +7,13 @@ export default class Testimonials extends React.Component {
 				<h1 class="pink">Sponsors</h1>
 				<h3>Co-Presenters</h3>
 				<div class="logos logos-big">
-					<img alt="Wideout Logo" src={require('../assets/sponsor-wideout.png')} />
-					<img alt="Saperium Logo" src={require('../assets/sponsor-saperium.png')} />
+					<img alt="Angat IO Logo" src={require('../assets/Angat_io_horizontal (1).png')} />
+					<img alt="ClinkIT Logo" src={require('../assets/clinkit-logo-horiontal-lightbg-xl.png')} />
 				</div>
 				{/*<h3>Major Sponsor</h3>
 				<div class="logos logos-big">
 				</div>*/}
-				<h3>Minor Sponsors</h3>
+				{/* <h3>Minor Sponsors</h3>
 				<div class="logos logos-small">
 					<img alt="Secret6 Logo" src={require('../assets/sponsor-secret6.png')} />
 					<img alt="Freelancer Logo" src={require('../assets/sponsor-freelancer-whitebg.png')} />	
@@ -25,11 +25,11 @@ export default class Testimonials extends React.Component {
 					<img alt="Samsung Logo" src={require('../assets/sponsor-samsung-whitebg.png')} />
 					<img alt="Tanda Logo" src={require('../assets/sponsor-tanda.png')} />
 					<img alt="Accenture Logo" src={require('../assets/sponsor-accenture.png')} />
-				</div>
+				</div> */}
 				<h1 class="pink">Partners</h1>
 				<div class="logos logos-small">
 					<img alt="Rizal Library Logo" src={require('../assets/partner-rizal-lib.png')} />
-					<img alt="UXPH Logo" src={require('../assets/partner-uxph.png')} />
+					{/* <img alt="UXPH Logo" src={require('../assets/partner-uxph.png')} /> */}
 				</div>
 			</div>
 		);
